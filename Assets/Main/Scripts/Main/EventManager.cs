4ec9d757e6610f61545e60ff19a54f5aa6e45d3f
aral burak dogan <aralburakdogan@gmail.com>
2023-09-24 10:30:38 +0300

    add : event manager for further usages

---
+++ Assets/Main/Scripts/Main/EventManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager 
+{
+    public enum EventSignature
+    {
+        //Add your event callouts here
+        OnGameStart, 
+        OnGameEnd, 
+        OnGamePause, 
+        OnGameResume,
+        OnPlayerDefeated,
+        OnPlayerWin,
+    }
+    private static readonly Dictionary<EventSignature, Action<object>> eventDictionary
+        = new();
+
+
+    public static void StartListening(EventSignature eventName, Action<object> listener)
+    {
+        if (eventDictionary.TryGetValue(eventName, out Action<object> thisEvent))
+        {
+            thisEvent += listener;
+            eventDictionary[eventName] = thisEvent;
+        }
+        else
+        {
+            thisEvent += listener;
+            eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(EventSignature eventName, Action<object> listener)
+    {
+        if (eventDictionary.TryGetValue(eventName, out Action<object> thisEvent))
+        {
+            thisEvent -= listener;
+            eventDictionary[eventName] = thisEvent;
+        }
+    }
+
+    public static void TriggerEvent(EventSignature eventName, object eventData = null)
+    {
+        if (eventDictionary.TryGetValue(eventName, out Action<object> thisEvent))
+        {
+            thisEvent.Invoke(eventData);
+        }
+    }
+}
