069cf93b074fc7d5c15725c16549f49f3d6cd9f7
aral burak dogan <aralburakdogan@gmail.com>
2023-09-28 21:57:58 +0300

    update : levels are redesigned

--- Assets/Main/Scripts/Config/LevelConfig.cs
+++ Assets/Main/Scripts/Config/LevelConfig.cs
@@ -6,7 +6,6 @@ public class LevelConfig : ScriptableObject
 {
     [field: SerializeField] public int LevelID { get; set; }
     [field: SerializeField] public Level LevelPrefab { get; set; }
-    [field: SerializeField] public float[] UpgradeTriggerPercentages { get; set; }
 }
 
 
